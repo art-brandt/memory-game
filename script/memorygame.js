5e80f134f619a0d00b7549202285cccccd508a86
art-brandt <art.brandt@ya.ru>
2018-08-03 00:36:26 +0600

    fixed shame

--- script/memorygame.js
+++ script/memorygame.js
@@ -1,30 +1,30 @@
 var score = 0;
 
-//создание функции перетосовки
-var shuffle = function () {
-  return Math.random() - 0.5;
-}
-
 var suits = ['C', 'D', 'H', 'S'];
 var pics = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'];
-var fullDeck = [];
-for (var j = 0; j < suits.length; j++) {
+const allCards = [];
+for (let j = 0; j < suits.length; j++) {
     for (var i=0; i < pics.length; i++) {
-        var card = pics[i] + suits[j];
-        fullDeck.push(card);
+        let cardName = pics[i] + suits[j];
+        allCards.push(cardName);
     }
 }
-fullDeck.sort(shuffle);
+
+allCards.sort(function() {
+    return Math.random() - 0.5
+});
 
 var doubleDeck = [];
-for (var i = 0; i < 18 / 2; i++) {
-    var fromFullDeck = fullDeck[i];
-    doubleDeck.push(fromFullDeck, fromFullDeck);
+for (let i = 0; i < 18 / 2; i++) {
+    let fromAllCards = allCards[i];
+    doubleDeck.push(fromAllCards, fromAllCards);
 }
-doubleDeck.sort(shuffle);
+doubleDeck.sort(function() {
+    return Math.random() - 0.5
+});
 
 var objCards = [];
-for(var i = 0; i < doubleDeck.length; i++) {    
+for(let i = 0; i < doubleDeck.length; i++) {    
     objCards[i] = {
         name: doubleDeck[i],
         bgPath: "url('./image/cards/" + doubleDeck[i] + ".png')",
@@ -32,20 +32,14 @@ for(var i = 0; i < doubleDeck.length; i++) {
     }
  }
 
-//Проверка сколько карт осталось
-var testLostCards = function() {
-    var value = $(".card").filter("empty-card");
-}
-
-//Попытка угадать
 var tryGame = function() {
     $(".card").on("click", function(event){
         $(this).removeClass("back");
         $(this).attr("data-flipped","true");
-        var clickedCards = $('.card').filter('[data-flipped="true"]');
+        let clickedCards = $('.card').filter('[data-flipped="true"]');
         if (clickedCards.length == 2) {
-            var firstClickedCardName = $(clickedCards[0]).attr("data-name");
-            var secondClickedCardName = $(clickedCards[1]).attr("data-name");
+            let firstClickedCardName = $(clickedCards[0]).attr("data-name");
+            let secondClickedCardName = $(clickedCards[1]).attr("data-name");
             $(".card").off("click");
             if (firstClickedCardName == secondClickedCardName) {
                 setTimeout(function(){ 
@@ -55,11 +49,19 @@ var tryGame = function() {
                     $(".score").text(score);
                     
                     if($(".empty-card").length == doubleDeck.length) {
-                        console.log("Игра окончилась");
-                        $("body").html('<div class="main-container"><img class="end-image" src="./image/Group.png"><p>Поздравляем!<br>Ваш итоговый счёт: <span class="score"></span> </p><a class="btn start-game-btn" href="./game.html">Ещё раз</a></div>');
-                        setTimeout(function(){
-                            $(".score").text(score);
-                                   }, 200);
+                        let xhr = new XMLHttpRequest();
+                        xhr.open("GET", 'endgame.html', true);
+                        xhr.send(null);
+                        xhr.onreadystatechange = function () {
+                            if (xhr.readyState == 4) {
+                                if(xhr.responseText) {
+                                    $("body").html(xhr.responseText);
+                                    setTimeout(function(){
+                                        $(".score").text(score);
+                                    }, 2);
+                                }
+                            }
+                        }
                     }
                     
                     tryGame();
@@ -78,29 +80,19 @@ var tryGame = function() {
         
 }
 
-//начинаем игру
+
 var startGame = function(inGame) {
     if (inGame == true) {
         $(".card").addClass('back');
-        console.log("Игра началась"); 
         tryGame();
     }
 }
 
 $(document).ready(function() {
-    for (var i = 0; i < doubleDeck.length; i++) {
+    for (let i = 0; i < doubleDeck.length; i++) {
         $('.cards-container').append('<div class="card" data-name="' + objCards[i].name +'"></div');
         $(".card:last").css("background-image", objCards[i].bgPath);
     }
     setTimeout(startGame, 5000, true);
     
 });
-
-
-
-
-
-
-
-
-
