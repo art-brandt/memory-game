015930cf13b3c5e7609783ef90846b905a9ab0fa
Mikhail Panteleev <art.brandt@ya.ru>
2018-09-04 18:44:48 +0600

    change js files and liketest changed

--- script/memorygame.js
+++ script/memorygame.js
@@ -3,6 +3,7 @@ var score = 0;
 var suits = ['C', 'D', 'H', 'S'];
 var pics = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'];
 const allCards = [];
+
 for (let j = 0; j < suits.length; j++) {
     for (var i=0; i < pics.length; i++) {
         let cardName = pics[i] + suits[j];
@@ -19,6 +20,7 @@ for (let i = 0; i < 18 / 2; i++) {
     let fromAllCards = allCards[i];
     doubleDeck.push(fromAllCards, fromAllCards);
 }
+
 doubleDeck.sort(function() {
     return Math.random() - 0.5
 });
@@ -35,21 +37,21 @@ for(let i = 0; i < doubleDeck.length; i++) {
 var tryGame = function() {
     $(".card").on("click", function(event){
         $(this).removeClass("back");
-        $(this).attr("data-flipped","true");
-        let clickedCards = $('.card').filter('[data-flipped="true"]');
+        $(this).attr("data-tid", "Card-flipped");
+        let clickedCards = $('.card').filter('[data-tid="Card-flipped"]');
         if (clickedCards.length == 2) {
             let firstClickedCardName = $(clickedCards[0]).attr("data-name");
             let secondClickedCardName = $(clickedCards[1]).attr("data-name");
             $(".card").off("click");
             if (firstClickedCardName == secondClickedCardName) {
                 setTimeout(function(){ 
-                    $(clickedCards[0]).addClass("empty-card").css("background-image","none").removeAttr("data-flipped").removeAttr("data-name");
-                    $(clickedCards[1]).addClass("empty-card").css("background-image","none").removeAttr("data-flipped").removeAttr("data-name");
+                    $(clickedCards[0]).addClass("empty-card").css("background-image","none").removeAttr("data-tid").removeAttr("data-name");
+                    $(clickedCards[1]).addClass("empty-card").css("background-image","none").removeAttr("data-tid").removeAttr("data-name");
                     score = score + $(".card").filter(".back").length * 42;
                     $(".score").text(score);
                     
                     if($(".empty-card").length == doubleDeck.length) {
-                        let xhr = new XMLHttpRequest();
+                        const xhr = new XMLHttpRequest();
                         xhr.open("GET", 'endgame.html', true);
                         xhr.send(null);
                         xhr.onreadystatechange = function () {
@@ -70,8 +72,8 @@ var tryGame = function() {
                 setTimeout(function(){
                     score = score - $(".card").filter(".back").length * 42;
                     $(".score").text(score);
-                    $(clickedCards[0]).attr("data-flipped","false").addClass('back');
-                    $(clickedCards[1]).attr("data-flipped","false").addClass('back');
+                    $(clickedCards[0]).attr("data-tid","Card").addClass('back');
+                    $(clickedCards[1]).attr("data-tid","Card").addClass('back');
                     tryGame();
                 }, 1400);
             }
@@ -90,7 +92,7 @@ var startGame = function(inGame) {
 
 $(document).ready(function() {
     for (let i = 0; i < doubleDeck.length; i++) {
-        $('.cards-container').append('<div class="card" data-name="' + objCards[i].name +'"></div');
+        $('.cards-container').append('<div class="card" data-name="' + objCards[i].name +'" data-tid="Card"></div');
         $(".card:last").css("background-image", objCards[i].bgPath);
     }
     setTimeout(startGame, 5000, true);
